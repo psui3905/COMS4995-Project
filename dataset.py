57bd6618da7ad6ec0912c9a22ac036eafeb5dc14
psui3905 <pw991110@gmail.com>
2022-12-04 15:07:54 -0500

    first commit

---
+++ dataset.py
@@ -0,0 +1,35 @@
+import torch, cv2
+import numpy as np
+import pandas as pd
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import *
+
+from preprocessing import *
+
+class SiimDataset(Dataset):
+    def __init__(self, df: pd.DataFrame):
+        super().__init__()
+        self.df = df
+        self.length = len(df)
+    
+    def __len__(self):
+        return self.length 
+        
+    def __getitem__(self, index):
+        # default img size used: 512x512
+        data = self.df.iloc[index]
+        image = cv2.imread('train/' + data.id + '.jpg')
+        label = np.array(data.label)
+        return image, label
+        
+if __name__ == '__main__':
+    meta_csv_path = './meta.csv'
+    train_csv_path = './siim-covid19-detection/train_image_level.csv'
+    convert_to_jpg(train_csv_path)
+    png_df = pd.read_csv(meta_csv_path)
+    dataset = SiimDataset(png_df)
+    img, label = dataset[0]
+    print(img)
+    # cv2.imshow('sample', img)
+    print(label)
---
+++ preprocessing.py
@@ -0,0 +1,77 @@
+
+import os, pydicom
+import pandas as pd 
+import numpy as np
+
+from PIL import Image
+from tqdm.auto import tqdm
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+
+
+def read_xray(path, voi_lut = True, fix_monochrome = True):
+    # Original from: https://www.kaggle.com/raddar/convert-dicom-to-np-array-the-correct-way
+    dicom = pydicom.read_file(path)
+    
+    # VOI LUT (if available by DICOM device) is used to transform raw DICOM data to 
+    # "human-friendly" view
+    if voi_lut:
+        data = apply_voi_lut(dicom.pixel_array, dicom)
+    else:
+        data = dicom.pixel_array
+
+    # depending on this value, X-ray may look inverted - fix that:
+    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = np.amax(data) - data
+        
+    data = data - np.min(data)
+    data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+    return data
+
+
+def resize(array, size, keep_ratio=False, resample=Image.LANCZOS):
+    # Original from: https://www.kaggle.com/xhlulu/vinbigdata-process-and-resize-to-image
+    im = Image.fromarray(array)
+    
+    if keep_ratio:
+        im.thumbnail((size, size), resample)
+    else:
+        im = im.resize((size, size), resample)
+    return im
+
+def convert_to_jpg(study_level_csv, img_size=512):
+    train_csv = pd.read_csv(study_level_csv)
+    image_id = []
+    dim0 = []
+    dim1 = []
+    splits = []
+    labels = []
+    
+
+    for split in ['test', 'train']:
+        save_dir = f'./jpg_form/{split}/'
+        os.makedirs(save_dir, exist_ok=True)
+    
+    for root, dirs, filenames in tqdm(os.walk(f'../siim-covid19-detection/{split}')):
+        for file in filenames:
+            # set keep_ratio=True to have original aspect ratio
+            csv_id = filenames.split('/')[0] + '_study'
+            id_row = train_csv.loc[train_csv['id'] == csv_id]
+            id_row = id_row.loc[:, id_row.columns != 'id']
+            label = id_row.values.tolist()
+            
+            xray = read_xray(os.path.join(root, file))
+            im = resize(xray, size=img_size)  
+            im.save(os.path.join(save_dir, file.replace('dcm', 'jpg')))
+
+            image_id.append(file.replace('.dcm', ''))
+            dim0.append(xray.shape[0])
+            dim1.append(xray.shape[1])
+            labels.append(label)
+            splits.append(split)
+            
+    df = pd.DataFrame.from_dict({'id': image_id, 'dim0': dim0, 'dim1': dim1, 'label': labels, 'split': splits})
+    df.to_csv('meta.csv', index=False)
+
+
+
